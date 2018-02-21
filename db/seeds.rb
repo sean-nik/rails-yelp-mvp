f47d8ddde40d60adc758fe984a5f690ffa5dc1e0
Sean Nikravesh <seannik@bluewin.ch>
2018-02-21 15:35:02 +0900

    review functionality added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: "McDonald's", address: '1-2-4 Ginza', category: 'french', phone_number: '080 5893 9384')
+Restaurant.create(name: "Wendy's", address: '1-5-2 Shibuya', category: 'belgian', phone_number: '080 4322 9564')
+Restaurant.create(name: "Tao Kim's", address: '2-2-44 Namba', category: 'chinese', phone_number: '080 5844 2224')
+Restaurant.create(name: "Leonardo", address: '2-2-2 Roppongi', category: 'italian', phone_number: '080 3843 1324')
+Restaurant.create(name: "Sushiro", address: '3-2-5 Gotanda', category: 'japanese', phone_number: '080 6523 9486')
